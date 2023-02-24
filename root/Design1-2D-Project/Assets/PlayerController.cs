ef4041b59abb8374e9561d900683644e46bc7732
Flynn Niccolo <headphonesgeek@gmail.com>
2023-02-23 19:27:02 -0500

    test level, sprite creation, created playercontroller

---
+++ root/Design1-2D-Project/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Vars
+    //PlayerController.Move
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
