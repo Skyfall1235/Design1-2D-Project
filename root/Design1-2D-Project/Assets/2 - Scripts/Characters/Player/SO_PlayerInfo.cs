622e92eed45970814e40f37a2a0f49092f8a9ae2
Skyfall1235 <113722636+Skyfall1235@users.noreply.github.com>
2023-03-08 02:00:14 -0500

    addition to public enums and attackplayer creation

--- root/Design1-2D-Project/Assets/2 - Scripts/Characters/Player/SO_PlayerInfo.cs
+++ root/Design1-2D-Project/Assets/2 - Scripts/Characters/Player/SO_PlayerInfo.cs
@@ -1,15 +1,26 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 1)]
 public class PlayerData : ScriptableObject
 {
     public Vector2 playerPosition;
-    public Vector2[] stageArray;
-    public int currentStage;
+    //is needed to return the payers posisiton to exactly where it was
+    public bool facingRight;
+    //this contains an array of an array. each LEVEL is the stagecameraarray, and each CAMERA position FOR THAT LEVEL is stored inside the inner array.
+    public Array[] stageCameraArray;
+    //this is required to hold the stage name and its camera positions
+    public int[] cameraLocation;
+    //this exists currently to potentially be used
     public int coinAmount;
+    //the base walkspeed of the player
     public float walkSpeed;
+    //the amount of FORCE applied to a DASH
     public float dashDistance;
+    //the amount of forced applied for a JUMP
     public float jumpForce;
+    //the current health value of the player
     public int currentHealth;
+    //the urrent weapontype
     public WeaponType weaponType;
 }
